c4cec3fb19e3631427189f72049215e99d943ff8
Visan Sebastian <emanuel-sebastian.visan-draghicescu@s.unibuc.ro>
2024-12-23 23:30:40 +0200

    Cargo build script
    Compiles the haskell and c shared libraries and links them to rust

--- examples-linking-rust-and-haskell/reverse_string/src/Reverse.hs
+++ examples-linking-rust-and-haskell/reverse_string/src/Reverse.hs
@@ -7,9 +7,9 @@ import Foreign.C.String
 foreign export ccall reverseString :: CString -> IO CString
 
 make_reverse :: String -> String
-make_reverse = foldl (\res s -> s:res) ""
+make_reverse = foldl (\res s -> s : res) ""
 
 reverseString :: CString -> IO CString
 reverseString cstr = do
-    str <- peekCString cstr
-    newCString (make_reverse str)
+  str <- peekCString cstr
+  newCString (make_reverse str)
