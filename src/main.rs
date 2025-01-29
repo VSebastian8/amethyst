e430885dea76afcbfd90a875f7907f3e72694af3
Visan Sebastian <emanuel-sebastian.visan-draghicescu@s.unibuc.ro>
2025-01-30 00:53:48 +0200

    Done Parser FFI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use amethyst::parser::parse_code;
+
 fn main() {
     println!("Amethyst - Turing Machine Programming Language");
+    let res = parse_code("automata acceptor = override(R, 4, '0'); automata main(acceptor a1){initial state q0 {0 / 0, R -> q0; _ / B, N -> final;} accept state final; }".to_owned());
+    println!("{:?}", res);
 }
